4eecb776f02362677a9d27d983a37c06aed394cb
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-11 14:11:32 +1100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<footer><center><h5>Copyright 2015 LaughingQuoll Admin Dashboard v2.4.4</h5></center></footer>
+<footer><center><h5>Copyright 2015 LaughingQuoll Admin Dashboard v2.5.0 Pre-Release 1</h5></center></footer>
