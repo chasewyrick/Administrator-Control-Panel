761ec9ec9abd6bb8994fe0864a7b9b6f9fb3e3fd
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-15 07:16:14 +1100

    Update index.php

--- messages/index.php
+++ messages/index.php
@@ -62,7 +62,7 @@ require '/admin/settings.php';
 <body>
 
     <div id="wrapper">
-       <?php require ''.$root.'/admin/nav.php'; ?>
+       <?php require '/admin/nav.php'; ?>
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
