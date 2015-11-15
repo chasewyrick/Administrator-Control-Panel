e21229571525a6400f86ccdc00c4d96725a88f7d
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-15 17:36:38 +1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'laughingquoll.net';
-$mysql_user = 'laughin1_admin';
-$mysql_pass = 'AwesomeL4C';
-$db = 'laughin1_admin';
+$host = 'hostname';
+$mysql_user = 'username';
+$mysql_pass = 'password';
+$db = 'database';
 $root = '/home/laughin1/public_html/'; /* This is your website address. Eg: /home/user/public_html/ */
-$loginplease = 'http://admin.laughingquoll.net'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
-$administrationemail = 'me@laughingquoll.net'; /* The email you want the message system to use as a reply and from email. */
+$loginplease = '../'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
+$administrationemail = 'email'; /* The email you want the message system to use as a reply and from email. */
 ?>
