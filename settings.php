23b300cd2733b8dfdf10f2340c1ef32d728d0358
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-15 17:36:57 +1100

    Removed Root
    Removed Useless Root Entry

--- settings.php
+++ settings.php
@@ -3,7 +3,6 @@ $host = 'hostname';
 $mysql_user = 'username';
 $mysql_pass = 'password';
 $db = 'database';
-$root = '/home/laughin1/public_html/'; /* This is your website address. Eg: /home/user/public_html/ */
 $loginplease = '../'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
 $administrationemail = 'email'; /* The email you want the message system to use as a reply and from email. */
 ?>
