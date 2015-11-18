2d3c75fa9f8575c31ab77a013558268400af90e9
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-18 19:11:24 +1100

    Update delete.php

--- tasks/delete.php
+++ tasks/delete.php
@@ -1,7 +1,7 @@
  <?php
  session_start();
 require'../settings.php';
-$id = isset($_GET['id']);
+$id = $_GET['id'];
 $conn = new mysqli($host, $mysql_user, $mysql_pass, $db);
 // Check connection
 if ($conn->connect_error) {
@@ -9,8 +9,7 @@ if ($conn->connect_error) {
 } 
 $sql = "DELETE FROM `tasks` WHERE `id` = $id";
 $result = $conn->query($sql);
-echo $result;
-echo 'Task has been deleted. <a href="/tasks">Back</a>';
+header("Location: /tasks");
 die();
 $conn->close();
 
