55c6c4ac4cbaeba61d5168c09cbfe725bf6cea9d
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-21 17:55:52 +1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,6 +3,5 @@ $host = '';
 $mysql_user = '';
 $mysql_pass = '';
 $db = '';
-$loginplease = '../'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
 $administrationemail = 'me@laughingquoll.net'; /* The email you want the message system to use as a reply and from email. */
 ?>
